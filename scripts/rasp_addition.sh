6a31b1a0ab48925a11a0aab3679695bfcdb2835a
Sebastiaan Beekman <sebas.beekman@gmail.com>
2024-04-05 12:11:16 +0200

    Stuff

--- scripts/rasp_addition.sh
+++ scripts/rasp_addition.sh
@@ -5,7 +5,6 @@
 python ../src/run.py \
      --dataset "add" \
      --dataset_size 20000 \
-     --min_length 1 \
      --max_length 4 \
      --d_var 4 \
      --n_heads_cat 4 \
@@ -21,4 +20,4 @@ python ../src/run.py \
      --output_dir "output/rasp/add/s0";
 # done          
 
-# --dataset "sort" --dataset_size 20000 --seed 0 --d_var 8 --n_heads_cat 4 --n_heads_num 4 --n_cat_mlps 2 --n_num_mlps 2 --n_layers 3 --one_hot_embed --count_only --save --save_code --device "cpu" --output_dir "output/rasp/sort/s0"
+# --dataset "add" --dataset_size 20000 --max_length 4 --d_var 4 --n_heads_cat 4 --n_heads_num 4 --n_cat_mlps 2 --n_num_mlps 2 --n_layers 3 --one_hot_embed --count_only --save --save_code --device "cpu" --output_dir "output/rasp/add/s0"
